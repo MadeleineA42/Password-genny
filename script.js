465566c2eb6bbffbca513b87e9caa61ccaf8c4bf
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-07-18 16:38:38 -0400

    added which num/char/letters can be included

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ function generatePassword() {
   console.log('button clicked');
   var length = prompt("Enter how many characters you would like. 8-128")
   if (length < 8 || length > 128) {
-      alert("ERROR! Password must be between 8 and 128 characters!");
-      // prompt to make sure password is correct amount of characters
-      return "You've generated a secure password";
-      // THIS WILL BE THE RETURN MESSAGE
-  } 
+    alert("ERROR! Password must be between 8 and 128 characters!");
+    // prompt to make sure password is correct amount of characters
+    return "You've generated a secure password";
+    // THIS WILL BE THE RETURN MESSAGE
+  }
   var includeLowercase = confirm("Would you like to include lowercase letters?");
   var includeUppercase = confirm("Would you like to include uppercase letters?");
   var includeSpecial = confirm("Would you like to include special characters?");
   var includeNumbers = confirm("Would you like to include numbers?");
   // these are vars for each prompt pop up
-  if (includeLowercase && includeUppercase && includeSpecial && includeNumbers);
+  if (includeLowercase && includeUppercase && includeSpecial && includeNumbers); {
     alert("You must include at least one character type.");
+  }
+
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  var numbers = "0123456789";
+
+
 }
+
 // added console so I can see when button is clicked
 
 // Write password to the #password input
