5a858d19aecdd1ef32b6e8e191dbd7e382295032
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-07-20 16:00:29 -0400

    added if statements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  console.log('button clicked');
+  console.log('button clicked');// added console so I can see when button is clicked
   var length = prompt("Enter how many characters you would like. 8-128")
   if (length < 8 || length > 128) {
     alert("ERROR! Password must be between 8 and 128 characters!");
@@ -16,18 +16,34 @@ function generatePassword() {
   var includeNumbers = confirm("Would you like to include numbers?");
   // these are vars for each prompt pop up
   if (includeLowercase && includeUppercase && includeSpecial && includeNumbers); {
-    alert("You must include at least one character type.");
+    // alert("You must include at least one character type.");
   }
 
   var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
   var numbers = "0123456789";
-  false = ("0")
-// this will help the prompt when you hit cancel
+  var characterSet = "";
+  // false = ("0")
+  // this will help the prompt when you hit cancel
+  if (includeLowercase) {
+    characterSet += lowercaseLetters;
+  }
+
+  if (includeUppercase) {
+    characterSet += uppercaseLetters;
+  }
+
+  if (includeSpecial) {
+    characterSet += specialChar;
+  }
+
+  if (includeNumbers) {
+    characterSet += numbers;
+  }
 }
 
-// added console so I can see when button is clicked
+
 
 // Write password to the #password input
 function writePassword() {
