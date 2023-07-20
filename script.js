7dfdadaad69f303308617e28552781ad678c37a8
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-07-20 17:41:18 -0400

    added comments and pointed out citation

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-// this is part of the starter code
+  // this is part of the starter code
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// this is also part of the starter code
 
 function generatePassword() {
   console.log('button clicked');// added console so I can see when button is clicked
@@ -28,7 +29,7 @@ function generatePassword() {
   var includeNumbers = confirm("Would you like to include numbers?");
   // these are vars for each prompt pop up
   if (includeLowercase && includeUppercase && includeSpecial && includeNumbers) {
-    
+
   }
 
   var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
@@ -52,13 +53,15 @@ function generatePassword() {
   if (includeNumbers) {
     characterSet += numbers;
   }
-  
+
   var password = "";
   for (var i = 0; i < length; i++) {
     var randIndex = Math.floor(characterSet.length * Math.random());
     password += characterSet[randIndex]
+    // this is the part I needed help with, I cited my source in the ReadMe
   }
   return password;
+  // this gives you a password in the password box!
 }
 
 
